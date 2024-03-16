e1e3846ba4753bdba5f2a97e984e2aed9c0672d1
Mesrop Daghbashyan <mesrop01@yahoo.com>
2024-03-16 07:33:39 +0100

    removed some dependencies

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
